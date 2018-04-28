0a57fa1c226997026d1e85558e2fc04ae5d94874
Louis Germe <louis.germe@epitech.eu>
2018-04-28 23:42:21 +0200

    debut routes  + gestion component

--- Animakids/src/app/app.module.ts
+++ Animakids/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
@@ -8,6 +11,30 @@ import { SigninComponent } from './connection/signin/signin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutComponent } from './dashboard/layout/layout.component';
 import { BodyComponent } from './dashboard/body/body.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'connection',
+    component: ConnectionComponent,
+    data: { title: 'Connexion' }
+  },
+  { path: '',
+    redirectTo: '/connection',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inscription',
+    component: SigninComponent,
+    data: { title: 'Inscription Utilisateur' }
+  },
+
+  {path: 'dashboard', component : DashboardComponent,
+  children:
+  [{path:'', component: BodyComponent},
+   {path:'body', component: BodyComponent},
+   ]},
+  { path: '**', component: ConnectionComponent }
+];
 
 @NgModule({
   declarations: [
@@ -20,7 +47,13 @@ import { BodyComponent } from './dashboard/body/body.component';
     BodyComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } // <-- debugging purposes only
+  ),
+  BrowserModule,
+  FormsModule,
+  HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
