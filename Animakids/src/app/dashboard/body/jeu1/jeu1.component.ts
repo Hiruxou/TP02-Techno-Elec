1feb67e1b326c15f5f4ea18fc042f0b87a68a015
bouche_2 <maxime1.boucher@epitech.eu>
2018-05-01 19:51:46 -0400

    finalisation jeu 1 + ajout du score

--- Animakids/src/app/dashboard/body/jeu1/jeu1.component.ts
+++ Animakids/src/app/dashboard/body/jeu1/jeu1.component.ts
@@ -4,6 +4,7 @@ import { Router } from "@angular/router";
 import { Observable } from 'rxjs/Observable';
 import { tap, catchError } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
+import { Text } from '@angular/compiler';
 
 @Component({
   selector: 'app-jeu1',
@@ -12,37 +13,83 @@ import { of } from 'rxjs/observable/of';
 })
 export class Jeu1Component implements OnInit {
   score: any;
+  result: any;
   imagetsons: any;
   did: number[];
-  response: any[];
+  response: number;
   imageUrl: any;
   x: number;
+  responses: String[];
 
 
   constructor(private http: HttpClient) { }
 
-  changeUrl() {
-    console.log(this.did.indexOf(this.x));
-    console.log(this.x);
-    console.log(this.did);
-     while (this.did.indexOf(this.x)!= -1)
-     {
-      this.x = Math.floor((Math.random() * 15));
-     }
-     
-      console.log("assets/images/"+this.imagetsons[this.x].image);
-     this.imageUrl = "assets/images/"+this.imagetsons[this.x].image;
-     this.did.push(this.x);
+  changeUrl(id) {
+    if (this.did.length == 10 && this.result == "")
+    {
+      if (id == this.response)
+        this.score++;
+      this.result = "ton score est de " + this.score + "/10";
     }
+    else if (this.result == "")
+    {
+      if (id == this.response)
+        this.score++;
+      while (this.did.indexOf(this.x)!= -1)
+      {
+        this.x = Math.floor((Math.random() * 15));
+      }
+      this.imageUrl = "assets/images/"+this.imagetsons[this.x].image;
+      this.did.push(this.x);
+      this.response = Math.floor((Math.random() * 4))
+      var y = Math.floor((Math.random() * 15))
+      this.responses.length = 0;
+      for (var i = 0; i < 4; i++)
+      {
+        if (i == this.response)
+        {
+          this.responses.push(this.imagetsons[this.x].image.toString().substring(0, this.imagetsons[this.x].image.toString().length -4));
+        }
+        else
+        {
+        while (y == this.x || this.responses.indexOf(this.imagetsons[y].image.toString().substring(0, this.imagetsons[y].image.toString().length - 4)) != -1)
+          {
+            y = Math.floor((Math.random() * 15));
+            }
+            this.responses.push(this.imagetsons[y].image.toString().substring(0, this.imagetsons[y].image.toString().length - 4));
+          }
+      }
+    }
+   }
 
   ngOnInit() {
     this.did = new Array();
+    this.responses = new Array();
+    this.result = "";
+    this.score = 0;
     this.http.get('/api/images').subscribe(data => {
       this.imagetsons = data;
       this.x = Math.floor((Math.random() * 15));
       this.imageUrl = "assets/images/"+this.imagetsons[this.x].image;
       this.did.push(this.x);
-
+      this.response = Math.floor((Math.random() * 4))
+      var y = Math.floor((Math.random() * 15))
+      this.responses.length = 0;
+      for (var i = 0; i < 4; i++)
+      {
+        if (i == this.response)
+        {
+          this.responses.push(this.imagetsons[this.x].image.toString().substring(0, this.imagetsons[this.x].image.toString().length -4));
+        }
+        else
+        {
+          while (y == this.x || this.responses.indexOf(this.imagetsons[y].image.toString().substring(0, this.imagetsons[y].image.toString().length - 4)) != -1)
+          {
+            y = Math.floor((Math.random() * 15));
+            }
+            this.responses.push(this.imagetsons[y].image.toString().substring(0, this.imagetsons[y].image.toString().length - 4));
+          }
+      }
     });
 
 
